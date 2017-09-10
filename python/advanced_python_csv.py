9c85aba7bf933838d7c12bde7172a2445c00e20a
Laura Howard <Laura@macs-MacBook-Pro.local>
2017-09-10 13:00:26 -0700

    Updated code

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,17 @@
-PLACE YOUR CODE HERE
+import sys
+import os
+import csv
+
+with open('faculty.csv') as f:
+    faculty = csv.reader(f, skipinitialspace=True)
+    next(faculty)
+    degrees = list()
+    for row in faculty:
+        degree = row[1]
+        degree = degree.replace(".","")
+        degrees.append(degree)
+    degrees = [i.split(' ') for i in degrees]
+    all_degrees = [i for sublist in degrees for i in sublist]
+    degree_count = {x:all_degrees.count(x) for x in all_degrees}
+    for i in sorted(degree_count.items(), key=lambda x: x[1], reverse=True):
+        print(i)
